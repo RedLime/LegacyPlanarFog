1bc9f77f51694c566d565da5f16774fcf2329c4f
RedLime <redlimerl@gmail.com>
2022-04-08 11:29:40 +0900

    remove fabric asm

--- src/main/java/com/redlimerl/zbufferfog/MixinSetup.java
+++ src/main/java/com/redlimerl/zbufferfog/MixinSetup.java
@@ -1,11 +1,13 @@
 package com.redlimerl.zbufferfog;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.impl.FabricLoaderImpl;
 import org.spongepowered.asm.mixin.Mixins;
 
-public class MixinSetup implements Runnable {
+public class MixinSetup implements ClientModInitializer {
+
     @Override
-    public void run() {
+    public void onInitializeClient() {
         String[] version = FabricLoaderImpl.INSTANCE.getGameProvider().getNormalizedGameVersion().split("\\.");
         System.out.println(version[0]+"."+version[1]);
         Mixins.addConfiguration("z-buffer-fog.mixins." + (version[0] + "." + version[1]) + ".json");
